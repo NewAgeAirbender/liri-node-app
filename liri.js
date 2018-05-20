1c5b68a4bde7b35f038146e3aa97fdfa136b2fe2
Rylie Johnson <paintedflame@gmail.com>
2018-05-19 23:02:15 -0500

    creates working movie-this function

--- liri.js
+++ liri.js
@@ -1,9 +1,13 @@
 //sets up the keys, twitter, and spotify
 require("dotenv").config();
 var keys = require('./keys.js');
+var request = require("request");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var fs = require("fs");
 
+//makes it easier to read
+var divider ="\n------------------------------------------------------------\n\n";
 
 var chosen = process.argv[2];
 //empty variable for multiple word movie or song title inputs
@@ -29,13 +33,14 @@ switch (chosen) {
         songIt(title);
         break;
     case 'movie-this':
-        movieIt();
+        movieIt(title);
         break;
     case 'do-what-it-says':
         doIt();
         break;
 }
 
+//doesn't fucking work. Something about the query parameters missing??
 function tweetIt() {
     var client = new Twitter(keys.twitter);
 
@@ -62,12 +67,12 @@ function songIt(song) {
     if (song != "") {
         spotify.search({ type: 'track', query: song }, function (error, data) {
             if (!error) {
-                console.log("------------------");
+                console.log(divider);
                 //prints artist, song name, preview url, and album name
                 for (var i = 0; i < data.tracks.items.length; i++) {
                     var songData = data.tracks.items[i];
                     console.log("Artist: " + songData.artists[0].name + "\nSong: " + songData.name + "\nURL: " + songData.preview_url + "\nAlbum: " + songData.album.name);
-                    console.log("----------------");
+                    console.log(divider);
                 }
             }
             else {
@@ -80,11 +85,11 @@ function songIt(song) {
         var special = { type: 'track', id: '3DYVWvPh3kGwPasp7yjahc' };
         spotify.lookup(special, function (error, data) {
             if (!error) {
-                console.log("------------------");
+                console.log(divider);
                 for (var i = 0; i < data.tracks.items.length; i++) {
                     var songData = data.tracks.items[i];
                     console.log("Artist: " + songData.artists[0].name + "\nSong: " + songData.name + "\nURL: " + songData.preview_url + "\nAlbum: " + songData.album.name);
-                    console.log("----------------");
+                    console.log(divider);
                 }
             }
             else {
@@ -92,4 +97,33 @@ function songIt(song) {
             }
         });
     }
+}
+
+//function to pull the movie shit
+function movieIt(title) {
+    //creates the query url
+    var URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + title;
+
+    //requests, saves the info, then logs it to file
+    request(URL, function(err, response, body) { 
+        var data = JSON.parse(body);
+        //console.log(data);
+
+        var movieData = [
+            "Movie: " + data.Title,
+            "Released: " + data.Released,
+            "IMDB Rating: " + data.Ratings[0].Value,
+            "Rotten Tomatos Rating: " + data.Ratings[1].Value,
+            "Country of Origin: " + data.Country,
+            "Language: " + data.Language,
+            "Plot: " + data.Plot,
+            "Players: " + data.Actors
+        ].join('\n');
+
+        fs.appendFile('log.txt', movieData + divider, function(err) {
+            if (err) throw err;
+            console.log(movieData);
+        });
+    });
+
 }
